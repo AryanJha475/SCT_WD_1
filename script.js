23e197ae9c5c919708c5f68016cc6188af278c11
AryanJha475 <aryanjha480@gmail.com>
2025-09-07 00:23:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
-// Navbar scroll effect
-window.addEventListener("scroll", function() {
-  let navbar = document.getElementById("navbar");
-  if (window.scrollY > 50) {
-    navbar.classList.add("scrolled");
-  } else {
-    navbar.classList.remove("scrolled");
-  }
+// Change navbar style on scroll
+window.addEventListener("scroll", function () {
+  const navbar = document.querySelector(".navbar");
+  navbar.classList.toggle("scrolled", window.scrollY > 50);
 });
